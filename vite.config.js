8fb1c114c46d2c698246bd08ab744b45e349a9e2
Angel Leal <alealx2@gmail.com>
2024-08-03 15:19:09 -0400

    explicit import vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
 import dotenv from 'dotenv';
+import path from 'path';
+
 dotenv.config();
 
 export default defineConfig({
   plugins: [hydrogen()],
+  resolve: {
+    alias: {
+      'vite': path.resolve(__dirname, 'node_modules/vite/dist/node/index.js'),
+    },
+  },
 });
