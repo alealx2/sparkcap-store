664917b65dbe35344bed8576964af1feb4aecc97
Angel Leal <alealx2@gmail.com>
2024-08-04 18:48:43 -0400

    normalize jsconfig

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
 import dotenv from 'dotenv';
-import path from 'path';
 
 dotenv.config();
 
 export default defineConfig({
-  plugins: [hydrogen()],
-  resolve: {
-    alias: {
-      'vite': path.resolve(__dirname, 'node_modules/vite/dist/node/index.js'),
-    },
-  },
+  plugins: [hydrogen()]
 });
